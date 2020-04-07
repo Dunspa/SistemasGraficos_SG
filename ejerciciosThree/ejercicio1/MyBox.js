4cae0c8edcb2510ead4f14a7e03a786069f26b69
Dunspa <jlgallego99@correo.ugr.es>
2020-04-07 21:11:03 +0200

    Nuevos ejemplos teoria

--- ejerciciosThree/ejercicio1/MyBox.js
+++ ejerciciosThree/ejercicio1/MyBox.js
@@ -2,7 +2,7 @@
 class MyBox extends THREE.Object3D {
    constructor(gui,titleGui) {
       super();
-
+            
       // Se crea la parte de la interfaz que corresponde a la caja
       // Se crea primero porque otros métodos usan las variables que se definen para la interfaz
       this.createGUI(gui,titleGui);
