4ec54c0ace9563429b02cb6107f5db08cf823658
Dunspa <jlgallego99@correo.ugr.es>
2020-04-07 22:21:12 +0200

    Completado ejercicio 6

---
+++ ejerciciosThree/ejercicio6/Modelo.js
@@ -0,0 +1,29 @@
+// Jose Luis Gallego Peña
+class Modelo extends THREE.Object3D {
+   constructor() {
+      super();
+
+      var that = this;
+      var materialLoader = new THREE.MTLLoader();
+      var objectLoader = new THREE.OBJLoader();
+      materialLoader.load ('Cat/12221_Cat_v1_l3.mtl',
+         function (materials) {
+            objectLoader.setMaterials(materials);
+            objectLoader.load('Cat/12221_Cat_v1_l3.obj',
+               function (object) {
+                  var modelo = object;
+                  modelo.rotateX(-Math.PI/2);
+                  modelo.scale.set(0.3, 0.3, 0.3);
+                  modelo.translateZ(-5);
+                  that.add(modelo);
+               }, null, null);
+         });
+   }
+
+   update() {
+      // Movimiento continuo del objeto
+      this.rotation.z += 0.01;
+      this.rotation.y += 0.01;
+      this.rotation.x += 0.01;
+   }
+}
---
+++ ejerciciosThree/ejercicio6/README.md
@@ -0,0 +1,3 @@
+# Ejercicio 6: Cargar un modelo en formato .obj
+
+Este ejercicio consiste en cargar un modelo en formato .obj
